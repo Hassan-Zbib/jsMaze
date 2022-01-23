823d1f84c7107a27b03edd89a193e70a67504ef3
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-01-23 16:06:57 +0200

    added event listeners

--- game.js
+++ game.js
@@ -1,28 +1,47 @@
-window.onload = function () {
+function startGame() {
+ 
+}
 
-    
+function reset() {
+
+}
+
+function win() {
+
+}
+
+function lose() {
+
+}
+
+function exit() {
+
+}
+
+
+function main() {
+
+    document.getElementById("game").addEventListener("mouseleave", exit)
+
     var start = document.getElementById("start")
     start.addEventListener("mouseover", startGame)
     start.addEventListener("click", reset)
 
     document.getElementById("end").addEventListener("mouseover", win)
 
-    function startGame() {
-
+    var boundaries = document.getElementsByClassName("boundary")
+    for ( var i = 0 ; i  < boundaries.length ; i++ ) {
+        boundaries[i].addEventListener("mouseover", lose)
     }
-
-    function reset() {
-
-    }
-
-    function win() {
-
-    }
-
-    function lose() {
-
-    }
-
-
+    
+    var score = 0
+    var score_container = document.getElementsByClassName("boundary example")[0]
+    score_container.style.padding = "5px 0 0 5px"
+    score_container.innerHTML = `your score: ${score}`
 
 }
+
+window.onload = main;
+
+
+
