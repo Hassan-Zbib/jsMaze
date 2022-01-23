ab1baa82a3c474a414e5674607f02e1af18a742f
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-01-23 17:56:40 +0200

    added event listeners controller

--- game.js
+++ game.js
@@ -1,32 +1,46 @@
 function startGame() {
     updateStatus("Dont Hit The Boundaries")
+    controlEventListeners(true)
 }
 
 function reset() {
     updateStatus("Begin by moving your mouse over the \"S\".")
     updateBoundaries()
+    controlEventListeners(true)
     updateScore(0)
 }
 
 function win() {
     updateStatus("You Won!")
     updateBoundaries(false)
+    controlEventListeners(false)
     updateScore(score + 5)
 }
 
 function lose() {
     updateStatus("You Lost!")
     updateBoundaries(true)
+    controlEventListeners(false)
     updateScore(score - 10)
 }
 
 function exit() {
     updateStatus("You're Out of Bound! Go Back to start")
+    controlEventListeners(false)
 } 
+
+function updateScore(score_value) {
+    score = score_value
+    document.getElementsByClassName("boundary example")[0].innerText = `Score: ${score}`
+}
+
+function updateStatus(text) {
+    document.getElementById("status").innerText = text
+}
 
 function updateBoundaries(status) {
     var boundaries = document.getElementsByClassName("boundary")
-    for ( var i = 0 ; i  < boundaries.length ; i++ ) {
+    for ( var i = 0 ; i  < boundaries.length -1 ; i++ ) {
         if (status === true) {
             boundaries[i].style.removeProperty("background-color")
             boundaries[i].classList.add("youlose")
@@ -39,39 +53,50 @@ function updateBoundaries(status) {
         }
     }
 }
-function updateScore(score_value) {
-    score = score_value
-    document.getElementsByClassName("boundary example")[0].innerText = `Score: ${score}`
-}
 
-function updateStatus(text) {
-    document.getElementById("status").innerText = text
-}
-
-
-function main() {
-
-    document.getElementById("game").addEventListener("mouseleave", exit)
+function controlEventListeners(add) {
 
     var start = document.getElementById("start")
     start.addEventListener("mouseover", startGame)
     start.addEventListener("click", reset)
 
-    document.getElementById("end").addEventListener("mouseover", win)
-
     var boundaries = document.getElementsByClassName("boundary")
-    for ( var i = 0 ; i  < boundaries.length - 1 ; i++ ) {
-        boundaries[i].addEventListener("mouseover", lose)
+
+    if (add === true) {
+        
+        document.getElementById("game").addEventListener("mouseleave", exit)
+
+        document.getElementById("end").addEventListener("mouseover", win)
+
+        for ( var i = 0 ; i  < boundaries.length - 1 ; i++ ) {
+            boundaries[i].addEventListener("mouseover", lose)
+        }
+    } else {
+
+        
+        document.getElementById("game").removeEventListener("mouseleave", exit)
+
+        document.getElementById("end").removeEventListener("mouseover", win)
+
+        for ( var i = 0 ; i  < boundaries.length - 1 ; i++ ) {
+            boundaries[i].removeEventListener("mouseover", lose)
+        }
+
     }
 
-    original_boundary_bg_color = window.getComputedStyle(boundaries[0]).backgroundColor
+
+}
+
+function main() {
+
+    controlEventListeners(true)
     
     var score_container = document.getElementsByClassName("boundary example")[0]
     score_container.style.padding = "5px 0 0 5px"
     updateScore(score)
 }
 
-var original_boundary_bg_color
+var original_boundary_bg_color = "#eeeeee"
 var green_boundary_bg_color = "#27e616"
 var score = 0
 
