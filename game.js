2d28fe11e30f935049fd7ae7b4b33f17ba79a38d
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-01-23 14:53:01 +0200

    added inital functions

---
+++ game.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+
+    
+    var start = document.getElementById("start")
+    start.addEventListener("mouseover", startGame)
+    start.addEventListener("click", reset)
+
+    document.getElementById("end").addEventListener("mouseover", win)
+
+    function startGame() {
+
+    }
+
+    function reset() {
+
+    }
+
+    function win() {
+
+    }
+
+    function lose() {
+
+    }
+
+
+
+}
